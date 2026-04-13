3505de3c837232f429d663689d8422554bb302a2
gillianrhett <gillianrhett@gmail.com>
2026-04-12 20:10:09 -0700

    starting milestone 1: search for goat

--- 210-lab-28.cpp
+++ 210-lab-28.cpp
@@ -9,15 +9,14 @@ using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
-int select_goat(set<Goat> trip);
-void delete_goat(set<Goat> &trip);
-void add_goat(set<Goat> &trip, string, int, string);
-void display_trip(set<Goat> trip);
+int select_goat(const set<Goat>&);
+void delete_goat(set<Goat>&);
+void add_goat(set<Goat>&, string, int, string);
+void display_trip(const set<Goat>&);
 int main_menu();
 
 int main() {
     srand(time(0));
-    //bool again; // I didn't use this, I just compare the input to 4
 
     // read & populate arrays for names and colors
     ifstream fin("names.txt");
@@ -32,10 +31,10 @@ int main() {
     fin1.close();
 
     // create a set for goats
-    set <Goat> myTrip;
+    set <Goat> goats;
 
     int choice = 0;
-    while (choice != 4) {
+    while (choice != 12) {
     // keep showing the menu after doing the action until user chooses 4 to quit
         choice = main_menu();
         if (choice == 1) { // add a goat
@@ -44,52 +43,74 @@ int main() {
             int rnd_age = rand() % (MAX_AGE + 1);
             string rnd_color = colors[rand() % (SZ_COLORS)];
             
-            add_goat(myTrip, rnd_name, rnd_age, rnd_color);
+            add_goat(goats, rnd_name, rnd_age, rnd_color);
         }
         if (choice == 2) { // delete a goat
-            delete_goat(myTrip);
+            delete_goat(goats);
         }
         if (choice == 3) { // list goats
-            display_trip(myTrip);
+            display_trip(goats);
         }
-        if (choice == 4) { // quit
+        if (choice == 4) { // search for a goat
+            
+        }
+        if (choice == 5) { // 
+            
+        }
+        if (choice == 6) { // 
+            
+        }
+        if (choice == 7) { // 
+            
+        }
+        if (choice == 8) { // 
+            
+        }
+        if (choice == 9) { // 
+            
+        }
+        if (choice == 10) { // 
+        }
+        if (choice == 11) { // 
+        }
+        if (choice == 12) { // quit
             cout << "\tGoodbye." << endl;
         }
     }
     return 0;
 }
 
-int select_goat(set<Goat> trip) {
+int select_goat(const set<Goat>& gs) {
 // return the index of a goat given the name input by user
     string nameIn; // user enters name of goat to delete
     cout << "Enter name of goat to delete: ";
     cin >> nameIn;
     int count = 0; // for checking whether we've reached the end of the set
     // search for a goat with this name
-    for(const Goat g : trip){
+    for(const Goat g : gs){
         if (g.get_name() == nameIn) 
             break;
         count++;
     }
-    if (count == trip.size()) {
+    if (count == gs.size()) {
         cout << "\tName not found." << endl;
         count = -1;
     }
     return count;
 }
 
-void delete_goat(set<Goat> &trip) {
+void delete_goat(set<Goat>& gs) {
 // prompt the user for a name and delete the first instance of the goat with that name
-    if(trip.size() == 0) {
+    if(gs.size() == 0) {
         cout << "The set is empty." << endl;
         return;
     }
-    int i = select_goat(trip); // i = index of the goat to delete
+    int i = select_goat(gs); // i = index of the goat to delete
     int cur = 0; // current index in the for loop
     if (i != -1) { // if the goat was found
-        for (Goat g : trip) {
+        for (Goat g : gs) {
             if (i == cur) {
-                trip.erase(g);
+                gs.erase(g);
                 return;
             }
             cur++;
@@ -97,20 +118,20 @@ void delete_goat(set<Goat> &trip) {
     }
 }
 
-void add_goat(set<Goat> &trip, string name, int age, string color ) {
+void add_goat(set<Goat>& gs, string name, int age, string color ) {
 // create a new goat and add it to the set
     Goat g(name, age, color);
-    trip.insert(g);
+    gs.insert(g);
 }
 
-void display_trip(set<Goat> trip) {
+void display_trip(const set<Goat>& gs) {
 // display a list of all the goats and their info
-    if(trip.size() == 0) {
+    if(gs.size() == 0) {
         cout << "The set is empty." << endl;
         return;
     }
     int count = 0; // for numbering the list of goats displayed
-    for(const Goat g : trip){
+    for(const Goat g : gs){
         count++;
         cout << "\t[" << count << "] " << g.get_name() << " (" << g.get_age() << ", " << g.get_color() << ")" << endl;
     }
@@ -123,13 +144,21 @@ int main_menu() {
     cout << "[1] Add a goat" << endl;
     cout << "[2] Delete a goat" << endl;
     cout << "[3] List goats" << endl;
-    cout << "[4] Quit" << endl;
-    while (!(choice >= 1 && choice <=4)) {
+    cout << "[4] Search for a goat" << endl;
+    cout << "[5] " << endl;
+    cout << "[6] " << endl;
+    cout << "[7] " << endl;
+    cout << "[8] " << endl;
+    cout << "[9] " << endl;
+    cout << "[10] " << endl;
+    cout << "[11] " << endl;
+    cout << "[12] Quit" << endl;
+    while (!(choice >= 1 && choice <=12)) {
         cout << "Choice --> ";
         try {
             cin >> choice;
-            if (cin.fail() || !(choice >= 1 && choice <=4))
-                throw invalid_argument("Invalid input. Enter a number 1, 2, 3, or 4.");
+            if (cin.fail() || !(choice >= 1 && choice <=12))
+                throw invalid_argument("Invalid input. Enter a number 1 to 12.");
         }
         catch(invalid_argument& e)
         {
